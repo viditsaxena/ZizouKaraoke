f24bef3b01b7241ee57234870119fcc90a4c12a8
Vidit Saxena <viditsax@gmail.com>
2015-10-28 20:14:33 -0400

    fixes timing

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,21 @@
 $(document).ready(function() {
   console.log("Loaded");
-  renderLyricsAgain();
-  // renderLyricsAgain();
+  $('.song').append('<li>');
+  renderIntroLyrics();
+  setTimeout(renderVerse1Lyrics, 31000);
+  setTimeout(renderChorusLyrics, 59500);
+  setTimeout(renderVerse2Lyrics, 92700);
+  setTimeout(renderChorusLyrics, 121000);
+  setTimeout(renderCoreLyrics, 152000);
 });
 
 
-var songLyrics = ['In 1972, under a scorching June Sun',
+var songIntro = ['In 1972, under a scorching June Sun',
 'In the French Coastal town of Marseilles',
 'Two Algerian immigrants awaited the birth of their 5th child',
-'Later that day, a star was born',
-'Christiano Ronaldo',
+'Later that day, a star was born']
+
+var songVerse1 = ['Christiano Ronaldo',
 'Wayne Rooney, Veron',
 'Suaraz, Van Basten',
 'Gianluigi Buffon',
@@ -23,12 +29,18 @@ var songLyrics = ['In 1972, under a scorching June Sun',
 'Jean-Pierre Papin',
 'Ballack, Van Persie',
 'Beckham, Giggs, Scholes',
-'But the strongest of them all',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'Lionel Messi',
+'But the strongest of them all']
+
+var songChorus = ['Zinedine Zidane',
+'Zinedine Zidane',
+'Superstar',
+'Superstar',
+'Zinedine Zidane',
+'Zinedine Zidane',
+'Superstar',
+'Superstar'];
+
+var songVerse2 = ['Lionel Messi',
 'Gareth Bale, Kewell',
 'Nedved, Maldini',
 'Aguero, Raul',
@@ -41,23 +53,17 @@ var songLyrics = ['In 1972, under a scorching June Sun',
 'Ronaldinho, Ronaldo, Romario, Rivaldo, Robinho, Ramirez, KAKA',
 'Falcao, Frank Ribery',
 'Pirlo, Cahill, Kompany',
-'But the strongest of them all',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'And he’ll burn through the dark like a fire (Puskas, Eusebio)',
+'But the strongest of them all'];
+
+var songCore = ['And he’ll burn through the dark like a fire (Puskas, Eusebio)',
 'So much more than the world was dreaming of wa ya ya (Beckenbauer, Platini, Best)',
 'He’ll fight (Charlton)',
 'And his light (Cruyff)',
 'will shine on and on and on (Baggio)',
 'Oh, why (Diego Armando Maradona)',
-'With his will to survive (Pele)',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'Zinedine Zidane, Zinedine Zidane',
-'Superstar, superstar',
-'Canavarro, Karagounis, Chicharito, William Gallas',
+'With his will to survive (Pele)']
+
+var songVerse3 = ['Canavarro, Karagounis, Chicharito, William Gallas',
 'Sanchez, Mark Bosnic, Hulk, Alexi Lalas',
 'Wilkshere, Honda, Busquets, Dickie, Simao',
 'Thierry Henry, Modric, Vidal',
@@ -66,16 +72,95 @@ var songLyrics = ['In 1972, under a scorching June Sun',
 'Zinedine Zidane, Zinedine Zidane',
 'He is a part of things, he is a part of things'];
 
-function renderLyricsAgain() {
+// function renderLyricsAgain() {
+//   var i = 0;
+//   $('.song').append('<li>');
+//   var intervalId = setInterval(function() {
+//   		if (i === songLyrics.length) {
+//   			clearInterval(intervalId);
+//   		} else {
+//     		$('li').text(songLyrics[i]).hide().slideDown(600);
+//         $('li').addClass('line');
+//     		i++;
+//       }
+//   }, 2500);
+// };
+
+function renderIntroLyrics() {
   var i = 0;
-  $('.song').append('<li>');
-  var intervalId = setInterval(function() {
-  		if (i === songLyrics.length) {
+  // $('.song').append('<li>');
+  var intervalId = setInterval(playIntro, 3500);
+
+  function playIntro(){
+  		if (i === songIntro.length) {
   			clearInterval(intervalId);
   		} else {
-    		$('li').text(songLyrics[i]).hide().slideDown(600);
+    		$('li').text(songIntro[i]).hide().slideDown(1700);
         $('li').addClass('line');
     		i++;
       }
-  }, 2500);
+  }
+};
+
+  function renderVerse1Lyrics() {
+    var i = 0;
+    // $('.song').append('<li>');
+    var intervalId = setInterval(playVerse, 2000);
+
+  function playVerse(){
+    if (i === songVerse1.length) {
+      clearInterval(intervalId);
+    } else {
+      $('li').text(songVerse1[i]).hide().slideDown(800);
+      $('li').addClass('line');
+      i++;
+    }
+  }
+};
+
+function renderVerse2Lyrics() {
+  var i = 0;
+  // $('.song').append('<li>');
+  var intervalId = setInterval(playVerse, 2000);
+
+function playVerse(){
+  if (i === songVerse2.length) {
+    clearInterval(intervalId);
+  } else {
+    $('li').text(songVerse2[i]).hide().slideDown(800);
+    $('li').addClass('line');
+    i++;
+  }
+}
+};
+function renderCoreLyrics() {
+  var i = 0;
+  // $('.song').append('<li>');
+  var intervalId = setInterval(playCore, 6000);
+
+  function playCore(){
+    if (i === songCore.length) {
+      clearInterval(intervalId);
+    } else {
+      $('li').text(songCore[i]).hide().slideDown(1000);
+      $('li').addClass('line');
+      i++;
+    }
+  }
+};
+
+function renderChorusLyrics() {
+  var i = 0;
+  // $('.song').append('<li>');
+  var intervalId = setInterval(playChorus, 3650);
+
+  function playChorus(){
+    if (i === songChorus.length) {
+      clearInterval(intervalId);
+    } else {
+      $('li').text(songChorus[i]).hide().slideDown(2700);
+      $('li').addClass('line');
+      i++;
+    }
+  }
 };
